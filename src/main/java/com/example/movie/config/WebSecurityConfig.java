f1b1e4e21f911af2445a96a7e407cfb1f1717aee
EliGarcia <garciaelisha93@yahoo.com>
2017-09-29 15:04:15 -0500

    added nessasary annotations

--- src/main/java/com/example/movie/config/WebSecurityConfig.java
+++ src/main/java/com/example/movie/config/WebSecurityConfig.java
@@ -1,16 +1,23 @@
 package com.example.movie.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import javax.sql.DataSource;
 
-public class WebSecurityConfig {
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http
